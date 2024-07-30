df0ead77630e6de2f99b2384a22cbd4f30df8698
JaeWangKim <139958282+JaeWangKim@users.noreply.github.com>
2024-07-31 00:36:47 +0900

    Title: 행복한 수열의 개수, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240731/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240731/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,13 +4,17 @@ mat = [list(map(int, input().split())) for _ in range(n)]
 
 total = 0  
 for i in range(1, n-m + 1):
-    count = 0
-    count2 = 0
+    count = 1
+    count2 = 1
     for k in range(m):
-        if mat[i-1][i+k-1] == mat[i-1][i+k]:
-            count +=2
-        if mat[i+k-1][i-1] == mat[i+k][i-1]:
-            count2 +=2
+        if m != 1:
+            if mat[i-1][i+k-1] == mat[i-1][i+k]:
+                count +=1
+            if mat[i+k-1][i-1] == mat[i+k][i-1]:
+                count2 +=1
+        else:
+            count = n
+            count2 = n
     if count >= m:
         total += 1
     if count2 >= m :

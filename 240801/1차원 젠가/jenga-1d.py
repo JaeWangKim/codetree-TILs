3ce5cfaeb98fb5e07eb484c71ca2d70bbc2c7ee1
JaeWangKim <139958282+JaeWangKim@users.noreply.github.com>
2024-08-01 11:47:27 +0900

    Title: 1차원 젠가, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240801/1차원 젠가/jenga-1d.py
@@ -0,0 +1,48 @@
+n = int(input())
+
+temp1 = [0]*n
+temp2 = [0]*n
+temp1_end = 0
+temp2_end = 0
+array = []
+for _ in range(n):
+    array.append(int(input()))
+# temp 만듬
+array.reverse()
+
+s1, e1 = map(int, input().split())
+s2, e2  = map(int, input().split())
+
+s1 -= 1
+e1 -= 1
+s2 -= 1
+e2 -= 1
+
+for i in range(s1, e1+1):
+    array[i] = 0
+
+for i in range(n):
+    if array[i] != 0:
+        temp1[temp1_end] = array[i]
+        temp1_end += 1
+
+for i in range(n):
+    array[i] = temp1[i]
+
+for i in range(s2, e2+1):
+    array[i] = 0
+
+for i in range(n):
+    if array[i] != 0:
+        temp2[temp2_end] = array[i]
+        temp2_end += 1
+    
+for i in range(n):
+    array[i] = temp2[i]
+
+array.reverse()
+
+print(len(list(a for a in array if a!=0)))
+for i in array:
+    if i !=0:
+        print(i)

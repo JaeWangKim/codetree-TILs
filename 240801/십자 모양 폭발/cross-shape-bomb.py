b81ddb79899cd3513f159957feb1d372318319fa
JaeWangKim <139958282+JaeWangKim@users.noreply.github.com>
2024-08-01 15:17:21 +0900

    Title: 십자 모양 폭발, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240801/십자 모양 폭발/cross-shape-bomb.py
+++ 240801/십자 모양 폭발/cross-shape-bomb.py
@@ -32,10 +32,12 @@ def boomb(arr, boomb_x, boomb_y):
     a -= 1
     #행에 대해서 먼저 제거하기
     for i in range(boomb_y-a, boomb_y+a+1):
-        arr[boomb_x][i] = 0
+        if i <= n:
+            arr[boomb_x][i] = 0
     #열에 대해 제거하기
     for i in range(boomb_x-a, boomb_x+a+1):
-        arr[i][boomb_y] =0
+        if i <= n:
+            arr[i][boomb_y] =0
 
     return arr
 

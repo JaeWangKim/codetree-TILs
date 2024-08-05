34d10d122fe995b49a9764f2988eb9e8e821d4fc
JaeWangKim <139958282+JaeWangKim@users.noreply.github.com>
2024-08-05 22:42:43 +0900

    Title: 알파벳과 사칙연산, Time: 5012ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

---
+++ 240805/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,32 @@
+n = input()
+arr = {}
+
+for i in range(0, len(n), 2):
+    arr[n[i]] = 1
+res = -(2**31)
+
+def compute_and_compare(curr_a):
+    global res
+    val = curr_a[n[0]]
+
+    for i in range(1, len(n), 2):
+        if n[i] == '+':
+            val += curr_a[n[i+1]]
+        if n[i] == '-':
+            val -= curr_a[n[i+1]]
+        if n[i] == '*':
+            val *= curr_a[n[i+1]]
+    res = max(res, val)
+
+def check_combs(curr_a):
+    compute_and_compare(curr_a)
+
+    for k in arr.keys():
+        if curr_a[k] >= 4:
+            continue
+        curr_a[k] += 1
+        check_combs(curr_a)
+        curr_a[k] -= 1
+    
+check_combs(arr)
+print(res)

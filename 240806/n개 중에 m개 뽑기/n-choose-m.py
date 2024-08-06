fcccce7a79d6ad0d59ffeb3007d0ec9a4ed7fbd1
JaeWangKim <139958282+JaeWangKim@users.noreply.github.com>
2024-08-06 10:55:11 +0900

    Title: n개 중에 m개 뽑기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240806/n개 중에 m개 뽑기/n-choose-m.py
@@ -0,0 +1,23 @@
+# n, m = map(int, input().split())
+# a = []
+
+# def choose(curr_a, cnt):
+#     if curr_a == m+1:
+
+
+#     for i in range(1,n+1):
+#         a.append(i)
+#         choose(curr_a+1, cnt)
+#         a.pop()
+    
+from itertools import combinations
+
+n, m = map(int, input().split())
+data = []
+for i in range(1, n+1):
+    data.append(i)
+
+
+result= list(combinations(data, m))
+for i, j in result:
+    print(i, j)

0386a85de4e2ed3b6016c24b7192f11b396a7ee3
JaeWangKim <139958282+JaeWangKim@users.noreply.github.com>
2024-08-06 22:05:27 +0900

    Title: 일렬로 서있는 소 2, Time: 183ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240806/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240806/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -1,11 +1,38 @@
+#from itertools import permutations
+
 n = int(input())
 
 arr = list(map(int, input().split()))
-count =0
+arr_mid = []
+count = 0
+visited= [False]*n
 
-for i in range(n):
-    for j in range(i+1, n):
-        for m in range(j+1, n):
-            if arr[i] <= arr[j] <= arr[m]:
-                count+=1
+
+def compare():
+    global count
+    if arr_mid[0] <= arr_mid[1] <= arr_mid[2]:
+        count += 1
+        return
+    else:
+        return
+
+def choose(curr_a, num):
+    if curr_a == 4:
+        compare()
+        return
+
+    for index, i in enumerate(arr):
+        if visited[index] == True or index<=num:
+            continue
+        arr_mid.append(i)
+        visited[index] = True
+
+        choose(curr_a+1, num+1)
+
+        arr_mid.pop()
+        visited[index] = False
+        if len(arr_mid) == 0:
+	        num += 1
+
+choose(1, 0)
 print(count)

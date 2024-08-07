7a96963381bfb74c561e175f938ad5d05a3cf81a
JaeWangKim <139958282+JaeWangKim@users.noreply.github.com>
2024-08-07 13:24:30 +0900

    Title: 체크판위에서 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240807/체크판위에서 2/on-the-checkboard-2.py
+++ 240807/체크판위에서 2/on-the-checkboard-2.py
@@ -1,5 +1,5 @@
 import sys
-input = sys.stdin.realine
+input = sys.stdin.readline
 r, c = map(int, input().split())
 
 array = [
@@ -13,9 +13,9 @@ for i in range(1,r):
     for j in range(1,c):
         for k in range(i+1, r-1):
             for l in range(j+1, c-1):
-                if array[0][0] != array[i][j] and 
-                array[i][j] != array[k][l] and
+                if array[0][0] != array[i][j] and \
+                array[i][j] != array[k][l] and \
                 array[k][l] != array[r-1][c-1]:
-                cnt += 1
+                    cnt += 1
 
 print(cnt)
